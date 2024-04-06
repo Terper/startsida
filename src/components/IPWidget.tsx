b0967e2a9c5056602dffbb225d6add17ce2ce920
Terper <jann.totterman@gmail.com>
2024-04-06 06:27:22 +0300

    ip api https

--- src/components/IPWidget.tsx
+++ src/components/IPWidget.tsx
@@ -28,7 +28,7 @@ type GetIP = {
 };
 
 const getIp = async () => {
-  const response = await fetch("http://ip-api.com/json/");
+  const response = await fetch("https://ip-api.com/json/");
   if (!response.ok) throw new Error("Tjänst urfnk");
   return await response.json();
 };
